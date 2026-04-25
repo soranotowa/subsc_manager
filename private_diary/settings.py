6a87f86f4981a4d15f95af95ff788cf3f1ef7448
towanosora <91195138+soranotowa@users.noreply.github.com>
2026-04-25 19:24:20 +0900

    fix default auto field

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'private_diary.wsgi.application'
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
