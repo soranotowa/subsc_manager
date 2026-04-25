e7343c37fae3633fff2307e1cc72e1919f37317d
towanosora <91195138+soranotowa@users.noreply.github.com>
2026-04-25 18:19:14 +0900

    disable manifest static

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'private_diary.urls'
 
 TEMPLATES = [
