d5b649c5d157e4c79d8742e3a5d7e321df5f669c
towanosora <91195138+soranotowa@users.noreply.github.com>
2026-04-30 00:04:02 +0900

    EMAIL_SETTINGS1

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -96,7 +96,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
